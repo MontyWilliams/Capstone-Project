b0bb8763808f3a0eb099c4d1a09aebdfa2d142e3
darbumas <2314@holbertonstudents.com>
2023-12-01 14:01:43 -0600

    server skeleton

---
+++ server/app.js
@@ -0,0 +1,4 @@
+/*jshint esversion: 9 */
+
+const campaignsRoutes = require('./routes/campaigns');
+app.use('/api/campaigns', campaignsRoutes);
---
+++ server/routes/campaigns.js
@@ -0,0 +1,17 @@
+/*jshint esversion: 9 */
+
+const express = require('express');
+const router = express.Router();
+
+// Get list of campaigns
+router.get('/', (req, res) => {
+  // ...
+});
+
+// Create a new campaign
+router.post('/', (req, res) => {
+  // ...
+});
+
+module.exports = router;
+

59055fdb7f04255725c0fc5da256486d59318f3e
Montywilliams <3650@holbertonschool.com>
2023-12-04 15:20:02 -0600

    add contract address

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import React, { useState } from 'react';
 import { ethers } from 'ethers';
 import Greeter from '/artifacts/contracts/Greeter.sol/Greeter.json'
 
+const greeterAddress = process.env.CONTRACT_ADDRESS
+
 function App() {
+
   return (
 
       <div></div>

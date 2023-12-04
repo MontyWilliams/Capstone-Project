1d3e3027a58c07b756ce6c02236f62149c17b089
Montywilliams <3650@holbertonschool.com>
2023-12-04 15:26:42 -0600

    create function that will creat an instance of the contract that we can interact withj

--- src/App.js
+++ src/App.js
@@ -5,6 +5,17 @@ import Greeter from '/artifacts/contracts/Greeter.sol/Greeter.json'
 const greeterAddress = process.env.CONTRACT_ADDRESS
 
 function App() {
+  const [greeting, setGreetingValue ] = useState("");
+  
+  // this function calls the greeting in the smart contract
+  async function fetchGreeting() {
+    if (typeof window.ethereum !== "undefined") {
+      // this const will call the wallet in the browser 
+      const provider = ethers.providers.Web3Provider(window.ethereum)
+      // this creates an instance of the contract, it needs the address, abi, and provider
+      const contract = new ethers.Contract(greeterAddress, Greeter.abi, provider)
+  }
+}
 
   return (
 

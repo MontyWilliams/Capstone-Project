8025b417101a12a805e43373644d23cbb2383fd9
Montywilliams <3650@holbertonschool.com>
2023-12-04 15:10:18 -0600

    import settings from main branch app

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,24 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-waffle");
+require("dotenv").config();
+
+const { API_URL, PRIVATE_KEY } = process.env;
+
+// eslint-disable-next-line no-undef
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+})
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.19",
+  networks: {
+    mumbai: {
+      url: API_URL,
+      accounts: [`0x${PRIVATE_KEY}`]
+    }
+  }
 };
---
+++ scripts/greeter.js
@@ -0,0 +1,39 @@
+const hre = require("hardhat");
+const ContractJson = require("../artifacts/contracts/Greeter.sol/Greeter.json")
+
+// This is tha binary interface which allows the front end to used all the functions within the contract
+const abi = ContractJson.abi;
+
+async function main() {
+
+  // use eThers to get the provider that will act as a bridge between our application and the contract which exists on the blockchain. IN the ethers documentation ther is a function for alchemy providers that specifies 'maticmum'
+  const alchemy = new hre.ethers.providers.AlchemyProvider(
+    'maticmum',
+    process.env.ALCHEMY_API_KEY
+  );
+  // the private key is used so that ethers can use our wallet to pay for the transactions
+  const userWallet = new hre.ethers.Wallet(process.env.PRIVATE_KEY, alchemy);
+
+  // ethers uses the contract address and the abi to understand where the contract is and what functiokns it contains 
+  const Greeter = new hre.ethers.Contract(
+    process.env.CONTRACT_ADDRESS,
+    abi,
+    userWallet
+  )
+
+  // this script will make to write transactions to modify the stake of the blockchain via our smart contract
+  const setTx1 = await Greeter.setGreeting("web3 is easy bro");
+  await setTx1.wait();
+  console.log("before: " + await Greeter.greet());
+
+  const setTx2 = await Greeter.setGreeting("web3 is even easier after completing a fu*****ng  2 year course!");
+  await setTx2.wait();
+  console.log("after: " + await Greeter.greet())
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
